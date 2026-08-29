9e55909aee97ff36fd37f974584ce3d93cb275c4
Oleg <olefinibrabus@gmail.com>
2026-08-29 23:44:19 +0300

    added mock test for can_access_google_page with return value type validation

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from unittest.mock import patch, MagicMock
+
+from app.main import can_access_google_page
+
+
+@patch("app.main.valid_google_url")
+@patch("app.main.has_internet_connection")
+def test_have_access_google_page(
+    mocked_internet_connection: MagicMock,
+    mocked_google: MagicMock,
+) -> None:
+    if any((
+            mocked_internet_connection.return_value is not True,
+            mocked_google.return_value is not True
+    )):
+        assert False
+
+    assert can_access_google_page("https://www.google.com") == "Accessible"
