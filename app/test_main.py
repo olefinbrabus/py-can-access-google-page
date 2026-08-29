5d27ef76ab47675b137da7dd89382fda133372c4
Oleg <olefinibrabus@gmail.com>
2026-08-30 00:04:44 +0300

    added deep refactoring for test_have_access_google_page

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,29 @@
+import pytest
 from unittest.mock import patch, MagicMock
+
 
 from app.main import can_access_google_page
 
 
 @patch("app.main.valid_google_url")
 @patch("app.main.has_internet_connection")
+@pytest.mark.parametrize(
+    "connection,google,result",
+    [
+        (True, True, "Accessible"),
+        (True, False, "Not accessible"),
+        (False, False, "Not accessible"),
+        (False, False, "Not accessible"),
+    ]
+)
 def test_have_access_google_page(
-    mocked_internet_connection: MagicMock,
-    mocked_google: MagicMock,
+        mocked_internet_connection: MagicMock,
+        mocked_google: MagicMock,
+        connection: bool,
+        google: bool,
+        result: str
 ) -> None:
-    if any((
-            mocked_internet_connection.return_value is not True,
-            mocked_google.return_value is not True
-    )):
-        assert False
+    mocked_internet_connection.return_value = connection
+    mocked_google.return_value = google
 
-    assert can_access_google_page("https://www.google.com") == "Accessible"
+    assert can_access_google_page("https://www.google.com") == result
