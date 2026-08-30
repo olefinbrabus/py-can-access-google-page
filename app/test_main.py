232611373c5fbf9ec3d090a9f0e66cf55c0e44c7
Oleg <olefinibrabus@gmail.com>
2026-08-30 11:00:19 +0300

    changed test name and variables order

--- app/test_main.py
+++ app/test_main.py
@@ -16,14 +16,13 @@ from app import main
         (False, False, "Not accessible"),
     ]
 )
-def test_have_access_google_page(
-        mocked_internet_connection: MagicMock,
+def test_can_access_google_page(
         mocked_google: MagicMock,
+        mocked_internet_connection: MagicMock,
         connection: bool,
         google: bool,
         result: str
 ) -> None:
     mocked_internet_connection.return_value = connection
     mocked_google.return_value = google
-
     assert main.can_access_google_page("https://www.google.com") == result
